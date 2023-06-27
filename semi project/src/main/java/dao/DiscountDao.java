2a0e8dce6993a12b78c09cce6691dcd231177604
ggyeah <ggyeah34@naver.com>
2023-06-27 14:11:51 +0900

    [임지예] 할인 Dao 추가 할인품목리스트, 상품번호에 따른 가격표출

--- semi project/src/main/java/dao/DiscountDao.java
+++ semi project/src/main/java/dao/DiscountDao.java
@@ -9,7 +9,7 @@ import vo.*;
 
 public class DiscountDao {
 
-	//1) 할인리스트
+	//1) 상품전체 + 할인리스트
 	public ArrayList<Discount> discountList(int beginRow, int rowPerPage) throws Exception {
 		ArrayList<Discount> dList = new ArrayList<>();
 		DBUtil dbUtil = new DBUtil();
@@ -145,34 +145,92 @@ public class DiscountDao {
 
 	    return totalCnt;
 	}
-	
 
-		
-	//6)할인품목 상세보기
-		public Discount discountOne(int productNo) throws Exception {
+	//6)할인적용 가격 가져오기
+		public Discount discountOneList(int productNo) throws Exception {
 			DBUtil dbUtil = new DBUtil();
 			Connection conn = dbUtil.getConnection();
-		    PreparedStatement discountOneStmt = conn.prepareStatement("select discount_no, product_no, discount_start, discount_end, discount_rate, createdate, updatedate from discount where product_no = ?");
-		    discountOneStmt.setInt(1, productNo);
-		    
-		    ResultSet discountOneRs = discountOneStmt.executeQuery();
-		    
-		   Discount discount = null;
-		    
-		   if(discountOneRs.next()) {
-	        	discount = new Discount();
-	        	discount.setDiscountNo(discountOneRs.getInt("discount_no"));
-	        	discount.setProductNo(discountOneRs.getInt("product_no"));
-	        	discount.setDiscountStart(discountOneRs.getString("discount_start"));
-	        	discount.setDiscountEnd(discountOneRs.getString("discount_end"));
-	        	discount.setDiscountRate(discountOneRs.getDouble("discount_rate"));
-	        	discount.setCreatedate(discountOneRs.getString("createdate"));
-	        	discount.setUpdatedate(discountOneRs.getString("updatedate"));
+		/*
+		SELECT
+		  CASE
+		    WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN d.discount_rate
+		    ELSE 0.0
+		  END AS discount_rate,
+		  CASE
+		    WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN p.product_price * (1 - d.discount_rate)
+		    ELSE p.product_price
+		  END AS discounted_price
+		FROM
+		  product p
+		LEFT JOIN
+		  discount d ON p.product_no = d.product_no
+		WHERE
+		  p.product_no = 7;
+		*/
+			PreparedStatement discountListStmt = conn.prepareStatement("SELECT CASE WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN d.discount_rate ELSE 0.0 END AS discount_rate, CASE WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN p.product_price * (1 - d.discount_rate) ELSE p.product_price END AS discounted_price FROM product p LEFT JOIN discount d ON p.product_no = d.product_no WHERE  p.product_no = ?");
+			discountListStmt.setInt(1, productNo); 
+	        ResultSet discountListRs = discountListStmt.executeQuery();
+	        
+	        Discount discount = null;
+	        
+	        if(discountListRs.next()) {
+	        	discount  = new Discount();
+	            discount.setDiscountRate(discountListRs.getDouble("discount_rate"));
+	            discount.setDiscountedPrice(discountListRs.getInt("discounted_price"));
 	    	}
-		    return discount;
-		}
+	        return discount;
+	    }
 
-		
+	//7) 할인적용된 리스트만
+		public ArrayList<Discount> discountinvokedList(int beginRow, int rowPerPage) throws Exception {
+			ArrayList<Discount> dList = new ArrayList<>();
+			DBUtil dbUtil = new DBUtil();
+			Connection conn = dbUtil.getConnection();
+
+			/*
+			SELECT
+			  p.product_no,
+			  p.category_name,
+			  p.product_name,
+			  p.product_status,
+			  p.product_stock,
+			  d.discount_start,
+			  d.discount_end,
+			  d.discount_no,
+			  CASE
+			    WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN d.discount_rate
+			    ELSE 0.0
+			  END AS discount_rate,
+			  CASE
+			    WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN p.product_price * (1 - d.discount_rate)
+			    ELSE p.product_price
+			  END AS discounted_price
+			FROM
+			  product p
+			INNER JOIN
+			  discount d ON p.product_no = d.product_no
+			*/
+			
+			PreparedStatement discountListStmt = conn.prepareStatement("SELECT p.product_no, p.category_name, p.product_name, p.product_status, p.product_stock, d.discount_start, d.discount_end, d.discount_no, CASE WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN d.discount_rate ELSE 0.0 END AS discount_rate, CASE WHEN (d.discount_start <= NOW() AND d.discount_end >= NOW()) THEN p.product_price * (1 - d.discount_rate) ELSE p.product_price END AS discounted_price FROM product p INNER JOIN discount d ON p.product_no = d.product_no");
+	        
+	        ResultSet discountListRs = discountListStmt.executeQuery();
+	        
+	        while(discountListRs.next()) {
+	            Discount discount = new Discount();
+	            discount.setProductNo(discountListRs.getInt("product_no"));
+	            discount.setCategoryName(discountListRs.getString("category_name"));
+	            discount.setProductName(discountListRs.getString("product_name"));
+	            discount.setProductStatus(discountListRs.getString("product_status"));
+	            discount.setProductStock(discountListRs.getInt("product_stock"));
+	            discount.setDiscountStart(discountListRs.getString("discount_start"));
+	            discount.setDiscountEnd(discountListRs.getString("discount_end"));
+	            discount.setDiscountRate(discountListRs.getDouble("discount_rate"));
+	            discount.setDiscountedPrice(discountListRs.getInt("discounted_price"));
+	            discount.setDiscountNo(discountListRs.getInt("discount_no"));
+	            dList.add(discount);
+	    	}
+	        return dList;
+	    }	
 }
 		    
 		    
